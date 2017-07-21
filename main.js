25c78edaa5cc174c1581520614cfdfc855b2ca94
Jordan Benner <jordanbenner@outlook.com>
2017-07-21 10:47:30 -0500

    Testing a prompt
    Commented out man url to test intro web page

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var hotText = 'MDN'
-var URL = 'https://developer.mozilla.org/';
+// var hotText = 'MDN'
+// var URL = 'https://developer.mozilla.org/';
+//
+// document.write('click to return to ' + hotText.link(URL));
 
-document.write('click to return to ' + hotText.link(URL));
+alert('Welcome to the page');
+var name = prompt('what is your name?')
+var favoriteColor = prompt('what is your favorite color?');
+
+document.write('Hello my name is ' + name + ' and my favorite color is ' + favoriteColor);
+
+console.log('Program Complete');
