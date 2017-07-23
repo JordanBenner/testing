492c0c6efa86a7dcbe40eba69e247c77248ba903
Jordan Benner <jordanbenner@outlook.com>
2017-07-23 17:40:09 -0500

    Test non browser
    Test for circle ci

--- main.js
+++ main.js
@@ -4,26 +4,26 @@
 // document.write('click to return to ' + hotText.link(URL));
 
 
-// function calc_age (age) {
-//   var yearsAlive = parseInt(age);
-//
-//   yearsAlive = yearsAlive * 365;
-//   return yearsAlive;
-// }
-//
-// exports.calc_age = calc_age;
+function calc_age (age) {
+  var yearsAlive = parseInt(age);
 
-var correctAnswer = false;
-var answer= prompt('what is 2+2?');
+  yearsAlive = yearsAlive * 365;
+  return yearsAlive;
+}
 
-  if (answer === '4'){
-  var correctAnswer = true;
-  }
-  else if (correctAnswer == true){
-  alert('good job the answer is indeed ' + answer);
-  document.write('you are a math wiz!');
-  }
-  else{
-    alert('sorry that is not the right answer.');
-    document.write('Please try again.')
-  }
+exports.calc_age = calc_age;
+
+// var correctAnswer = false;
+// var answer= prompt('what is 2+2?');
+//
+//   if (answer === '4'){
+//   var correctAnswer = true;
+//   }
+//   else if (correctAnswer == true){
+//   alert('good job the answer is indeed ' + answer);
+//   document.write('you are a math wiz!');
+//   }
+//   else{
+//     alert('sorry that is not the right answer.');
+//     document.write('Please try again.')
+//   }
